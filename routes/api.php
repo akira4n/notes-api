f689fe57e9ae9f2b8c163095417cd2413c0d1974
akira4n <yobiijo94@gmail.com>
2026-02-27 14:11:40 +0700

    feat(api): add crud api routes for notes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\NoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,19 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function (){
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // get all notes
+    Route::get('/notes', [NoteController::class, 'index']);
+
+    // get specific note by id 
+    Route::get('/notes/{id}', [NoteController::class, 'show']);
+
+    // store new notes
+    Route::post('/notes', [NoteController::class, 'store']);
+
+    // edit note
+    Route::patch('/notes/{id}', [NoteController::class, 'update']);
+    
+    // delete note
+    Route::delete('/notes/{id}', [NoteController::class, 'destroy']);
 });
